83a3613b4ffcb953a88f605bb30044f6a8fc7a35
Pablo de Haro <pablitar@gmail.com>
2015-07-03 23:13:35 -0300

    Updating config

--- fun-spaces/test/ar/com/pablitar/funspaces/TraitsTest.scala
+++ fun-spaces/test/ar/com/pablitar/funspaces/TraitsTest.scala
@@ -3,15 +3,26 @@ package ar.com.pablitar.funspaces
 import org.junit.Test
 import org.junit.Assert._
 
-import Spaces.Space3D._
+
 
 class TraitsTest {
   
   @Test
   def aCarShouldChangePositionWithSpeed() {
+    import Spaces.Space3D._
     val aCar = new Car(point(10,10,10))
     aCar.applySpeed(0.1)
     assertEquals(point(1,1,1),aCar.position) 
+  }
+  
+  @Test
+  def aCarShouldChangePositionWithSpeedIn2D() {
+    import Spaces.Space2D._
+    val aPoint = point(10,10)
+    val aCar = new Car(aPoint)
+    aCar.applySpeed(0.1)
+    Spaces.Space3D.point(10,23,2) + aPoint
+    assertEquals(point(1,1),aCar.position) 
   }
 
 }
